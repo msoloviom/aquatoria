9d322eb9eaf9cc5f9c774c64513c526d52451767
msoloviom <marinaby@i.ua>
2018-02-08 01:19:11 +0200

    initial commit

---
+++ src/main/java/com/ds/aquatoria/AquatoriaApplication.java
@@ -0,0 +1,12 @@
+package com.ds.aquatoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquatoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquatoriaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ds/aquatoria/AquatoriaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ds.aquatoria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AquatoriaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
